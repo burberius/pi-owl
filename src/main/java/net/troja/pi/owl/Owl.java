db73cdd43751a74b1eb663deb9251af6bb56ea4a
Jens Oberender <burberius@users.noreply.github.com>
2018-10-16 21:41:24 +0200

    Added alarming feature.

--- src/main/java/net/troja/pi/owl/Owl.java
+++ src/main/java/net/troja/pi/owl/Owl.java
@@ -15,41 +15,80 @@ import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 
-public class Owl implements GpioPinListenerDigital {
+import static org.quartz.CronScheduleBuilder.cronSchedule;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Owl implements Job, GpioPinListenerDigital {
     private final GpioController gpio = GpioFactory.getInstance();
     private final DateFormat dateFormat = new SimpleDateFormat("H-m");
     private GpioPinDigitalInput button;
     private GpioPinDigitalOutput leftEye;
     private GpioPinDigitalOutput rightEye;
     private final Random rand = new Random(System.currentTimeMillis());
-    private Process sayProcess;
     private boolean working;
     private long last;
 
-    public void setup() {
+    private static boolean alarming;
+    private static Process mp3Process;
+
+    public void setup() throws SchedulerException {
         button = gpio.provisionDigitalInputPin(RaspiPin.GPIO_11, "Start Button", PinPullResistance.PULL_UP);
         button.addListener(this);
         leftEye = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "Left Eye", PinState.LOW);
         leftEye.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
         rightEye = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_12, "Right Eye", PinState.LOW);
         rightEye.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
-        System.out.println("Setup");
+
+        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+        JobDetail job = newJob(Owl.class)
+                .withIdentity("myJob", "group1")
+                .build();
+        Trigger trigger = newTrigger()
+                .withIdentity("myTrigger", "group1")
+                .withSchedule(cronSchedule("0 45 6 ? * Mon-Fri"))
+                .forJob("myJob", "group1")
+                .build();
+        scheduler.scheduleJob(job, trigger);
+        scheduler.start();
+
+        System.out.println("Setup " + scheduler.isStarted());
+    }
+
+    public void execute(JobExecutionContext jobExecutionContext) {
+        System.out.println("Alarm start");
+        Owl.alarming = true;
+        playMp3("alarm.mp3");
+        try {
+            Owl.mp3Process.waitFor();
+        } catch (InterruptedException e) {
+        }
+        Owl.alarming = false;
+        System.out.println("Alarm end");
     }
 
     public void handleGpioPinDigitalStateChangeEvent(final GpioPinDigitalStateChangeEvent event) {
         if (event.getState() == PinState.HIGH || working == true) {
             return;
         }
+        if(Owl.alarming) {
+            System.out.println("Alarm stop");
+            Owl.mp3Process.destroy();
+            Owl.alarming = false;
+            return;
+        }
         working = true;
         try {
             if (System.currentTimeMillis() - last < 5000) {
-                say("ungeduld.mp3");
+                playMp3("ungeduld.mp3");
                 bothEyesBlink(1000);
             } else {
                 last = System.currentTimeMillis();
                 final int number = rand.nextInt(5);
-                say(dateFormat.format(new Date()) + ".mp3");
+                playMp3(dateFormat.format(new Date()) + ".mp3");
 
                 switch (number) {
                 case 0:
@@ -74,15 +113,14 @@ public class Owl implements GpioPinListenerDigital {
 
             }
         } catch (final InterruptedException e) {
-
         }
         working = false;
     }
 
-    private void say(final String mp3) {
+    private void playMp3(final String mp3) {
         final ProcessBuilder builder = new ProcessBuilder("mpg123", mp3, ">", "/dev/null");
         try {
-            sayProcess = builder.start();
+            Owl.mp3Process = builder.start();
         } catch (final IOException e) {
             System.err.println("Could not play mp3: " + e.getMessage());
         }
@@ -91,13 +129,13 @@ public class Owl implements GpioPinListenerDigital {
     private void bothEyes(final int milis) throws InterruptedException {
         leftEye.high();
         rightEye.high();
-        sayProcess.waitFor();
+        Owl.mp3Process.waitFor();
         leftEye.low();
         rightEye.low();
     }
 
     private void bothEyesBlink(final int milis) throws InterruptedException {
-        while (sayProcess.isAlive()) {
+        while (Owl.mp3Process.isAlive()) {
             leftEye.high();
             rightEye.high();
             Thread.sleep(milis);
@@ -108,27 +146,27 @@ public class Owl implements GpioPinListenerDigital {
     }
 
     private void leftEyeBlink(final int milis) throws InterruptedException {
-        while (sayProcess.isAlive()) {
+        while (Owl.mp3Process.isAlive()) {
             leftEye.pulse(milis, true);
             Thread.sleep(milis);
         }
     }
 
     private void rightEyeBlink(final int milis) throws InterruptedException {
-        while (sayProcess.isAlive()) {
+        while (Owl.mp3Process.isAlive()) {
             rightEye.pulse(milis, true);
             Thread.sleep(milis);
         }
     }
 
     private void leftRightEyeBlink(final int milis) {
-        while (sayProcess.isAlive()) {
+        while (Owl.mp3Process.isAlive()) {
             leftEye.pulse(milis, true);
             rightEye.pulse(milis, true);
         }
     }
 
-    public static void main(final String... args) throws InterruptedException {
+    public static void main(final String... args) throws InterruptedException, SchedulerException {
         final Owl owl = new Owl();
         owl.setup();
         while (true) {
